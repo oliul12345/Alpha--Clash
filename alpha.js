4a372b443c0e8a5f0640fa69822f87a1e8110e3d
unknown <shikdermdoliullah@gmail.com>
2024-02-15 18:55:05 +0600

    again play section add

--- alpha.js
+++ alpha.js
@@ -20,8 +20,62 @@ function continueGame(){
     setBackgroundColorById(alphabet);
 }
 
+
+////   keyBoard events below here //////////
+function handleKeyUp(event){
+    const expectedKey = event.key
+
+    console.log('expected key is',expectedKey)
+    const currentPlayerKey = document.getElementById('current-alphabet')
+    const currentPlayerKeyText = currentPlayerKey.innerText;
+    const currentPlayerKeyTextConvert = currentPlayerKeyText.toLowerCase()
+    console.log('current is', currentPlayerKeyTextConvert)
+
+    if(expectedKey === currentPlayerKeyTextConvert){
+        console.log('You got a point');
+        // get current score //////////
+        const getCurrentScoreId = document.getElementById('score-id')
+        const getCurrentScoreText = getCurrentScoreId.innerText;
+        const currentScore = parseInt(getCurrentScoreText)
+        console.log(typeof(currentScore))
+        ////  set the new score ////////
+        const newScore = currentScore + 1 ;
+        getCurrentScoreId.innerText = newScore;
+        console.log(newScore)
+
+        removeBackgroundColorById(currentPlayerKeyTextConvert)
+        continueGame()
+    }
+    else{
+        console.log('dur o mia ke korso')
+        ///  get the life text 
+        const currentLifeId = document.getElementById('life-id');
+        const getCurrentLife = currentLifeId.innerText;
+        const currentLife = parseInt(getCurrentLife);
+        console.log('current life',currentLife)
+        ///  set the life after decrease 
+        const newLife = currentLife - 1;
+        currentLifeId.innerText = newLife;
+
+        if(newLife === 0){
+            console.log('game over')
+            hideElementById('play-ground');
+            showElementById('show-score');
+        }
+    }
+
+}
+document.addEventListener('keyup',handleKeyUp)
+
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();
+}
+
+function playAgain(){
+    hideElementById('show-score');
+    showElementById('home-screen');
+    
 }
